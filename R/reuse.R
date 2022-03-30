dbc95209a2e0b87dc944f15863ae349569e13e6d
Mauro Lepore <maurolepore@gmail.com>
2022-03-30 15:21:36 -0300

    reuse() supports overwrite

--- R/reuse.R
+++ R/reuse.R
@@ -1,6 +1,26 @@
-reuse <- function(x, name, board) {
-  if (!pins::pin_exists(board, name = name)) {
+#' Reuse R objects if possible
+#'
+#' @inheritParams pins::pin_write
+#' @param overwrite Logical. Overwrite the cache?
+#'
+#' @return The R object written to or read from the cache.
+#' @export
+#'
+#' @examples
+#' one <- 1 |> reuse("one")
+#' two <- 2 |> reuse("one")
+#' two
+#'
+#' two <- 2 |> reuse("one", overwrite = TRUE)
+#' two
+reuse <- function(x,
+                  name,
+                  board = pins::board_folder(rappdirs::user_cache_dir("reuse")),
+                  overwrite = getOption("reuse.overwrite", default = FALSE)) {
+  if (overwrite || !pins::pin_exists(board, name = name)) {
     pins::pin_write(board, x = x, name = name)
   }
   pins::pin_read(board, name = name)
 }
+
+reuse_board <- function() pins::board_folder(rappdirs::user_cache_dir("reuse"))
--- tests/testthat/test-reuse.R
+++ tests/testthat/test-reuse.R
@@ -12,15 +12,32 @@ test_that("writes to a given folder", {
   expect_true(pins::pin_exists(board, "one"))
 })
 
-test_that("if x exists in the board, it's reused not recomputed", {
+test_that("if x already exists in the board, it's reused not recomputed", {
   board <- pins::board_temp()
-  x <- 1
-  x |> reuse("one", board = board)
-  pins::pin_read(board, "one")
+  one <- 1 |> reuse("one", board = board)
+  two <- 2 |> reuse("one", board = board)
+  expect_equal(two, 1)
+})
 
-  x <- 2
-  one <- x |> reuse("one", board = board)
+test_that("defaults to using the cache of the reuse package", {
+  on.exit(pins::pin_delete(board, "one"), add = TRUE, after = FALSE)
 
-  expect_equal(one, 1)
-  expect_equal(pins::pin_read(board, "one"), 1)
+  reuse(1, "one")
+  file <- fs::path(rappdirs::user_cache_dir("reuse"), "one.qs")
+  board <- pins::board_folder(rappdirs::user_cache_dir("reuse"))
+  expect_true(pins::pin_exists(board, "one"))
+})
+
+test_that("you can overwrite the cache with an option", {
+  withr::local_options(list(reuse.overwrite = TRUE))
+  one <- 1 |> reuse("one")
+  two <- 2 |> reuse("one")
+  expect_equal(two, 2)
+})
+
+test_that("you can overwrite the cache", {
+  on.exit(pins::pin_delete(reuse_board(), "one"))
+  one <- 1 |> reuse("one")
+  two <- 2 |> reuse("one", overwrite = TRUE)
+  expect_equal(two, 2)
 })
