7e871cf0c9ddc091ec1fdf19acf3b21eaa1896a9
julie-vienne <j.vienne@hotmail.com>
2022-04-08 15:10:23 +0200

    Add failing test due to lacking definition of new object

--- tests/testthat/test-new_name.R
+++ tests/testthat/test-new_name.R
@@ -6,3 +6,13 @@ test_that("new_name() works with no error", {
   none <- NA
   expect_error(new_name("old", "new"), none)
 })
+
+test_that("new_name() creates 'new' with no error ", {
+  board <- pins::board_temp()
+  reuse(1, "old", board)
+
+  none <- NA
+  expect_error(new_name("old", "new"), none)
+
+  expect_true(pins::pin_exists(board = board, "new"))
+})
