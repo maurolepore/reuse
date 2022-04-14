9798b0eabed83e74732e13c1911e3772ad72108a
julie-vienne <j.vienne@hotmail.com>
2022-04-14 15:53:41 +0200

    Update function new_name to create a new pin

--- R/new_name.R
+++ R/new_name.R
@@ -1,4 +1,17 @@
-new_name <- new_name <- function(old, new, board) {
+new_name <- function(old, new, board) {
 
+  board <- board
+
+  old_name <- deparse(substitute(old))
+
+  new_pin <- pins::pin_read(board, old_name)
+
+  new_name <- deparse(substitute(new))
+
+  new <- reuse(new_pin, new_name, board)
+
+  test <- expect_true(pins::pin_exists(board = board, "new"))
+
+  return(test)
 }
 
