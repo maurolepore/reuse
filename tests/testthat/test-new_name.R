5254e388bf66309436426891606f01139efebb47
julie-vienne <j.vienne@hotmail.com>
2022-04-14 15:54:57 +0200

    Fix arguments to make tests pass

--- tests/testthat/test-new_name.R
+++ tests/testthat/test-new_name.R
@@ -4,7 +4,7 @@ test_that("new_name() works with no error", {
   expect_true(pins::pin_exists(board = board, "old"))
 
   none <- NA
-  expect_error(new_name("old", "new"), none)
+  expect_error(new_name(old, new, board), none)
 })
 
 test_that("new_name() creates 'new' with no error ", {
@@ -12,14 +12,14 @@ test_that("new_name() creates 'new' with no error ", {
   reuse(1, "old", board)
 
   none <- NA
-  expect_error(new_name("old", "new"), none)
+  expect_error(new_name(old, new, board), none)
 
   expect_true(pins::pin_exists(board = board, "new"))
 })
 
 test_that("new_name() takes a `board`", {
   board <- pins::board_temp()
-
+  reuse(1, "old", board)
   none <- NA
-  expect_error(new_name("old", "new", board = board), none)
+  expect_error(new_name(old, new, board = board), none)
 })
