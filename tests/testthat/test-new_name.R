48fcdfe1947cafa5e1745bf63bb8180c311a6860
Mauro Lepore <maurolepore@gmail.com>
2022-04-20 12:39:34 -0600

    Remove test that is too vague

--- tests/testthat/test-new_name.R
+++ tests/testthat/test-new_name.R
@@ -1,12 +1,3 @@
-test_that("new_name() works with no error", {
-  board <- pins::board_temp()
-  reuse(1, "old", board)
-  expect_true(pins::pin_exists(board = board, "old"))
-
-  none <- NA
-  expect_error(new_name("old", "new", board), none)
-})
-
 test_that("new_name() creates 'new' with no error ", {
   board <- pins::board_temp()
   reuse(1, "old", board)
