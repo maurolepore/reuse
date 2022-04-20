dc613b6bb10d2bf6b1084bdc1b558e84ba1a4e1f
Mauro Lepore <maurolepore@gmail.com>
2022-04-20 07:41:26 -0600

    Add documentation

--- R/new_name.R
+++ R/new_name.R
@@ -1,3 +1,17 @@
+#' Title
+#'
+#' @inheritParams reuse
+#' @param old String. The name of the old object.
+#' @param new String. The name of the new object.
+#'
+#' @return The object stored as 'new'.
+#' @export
+#'
+#' @examples
+#' board <- pins::board_temp()
+#' reuse(1, "old", board)
+#' new_name("old", "new", board)
+#' reuse(name = "new")
 new_name <- function(old, new, board) {
   new_pin <- pins::pin_read(board, old)
   new <- reuse(new_pin, new, board)
