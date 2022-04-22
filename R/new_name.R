5af8a2b00b24ee6e7f5056d615cd7332999bb860
julie-vienne <j.vienne@hotmail.com>
2022-04-22 19:12:54 +0200

    Add default board in new_name

--- R/new_name.R
+++ R/new_name.R
@@ -18,7 +18,7 @@
 #' "old" %>% new_name("new", board)
 #' pin_exists("old", board = board)
 #' pin_exists("new", board = board)
-new_name <- function(old, new, board) {
+new_name <- function(old, new, board = board_reuse()) {
   new_pin <- pins::pin_read(board, old)
   new <- reuse(new_pin, new, board)
 
