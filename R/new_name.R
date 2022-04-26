7d121887e40ef788d15c51b6a75b2cab03936933
julie-vienne <67040704+julie-vienne@users.noreply.github.com>
2022-04-26 13:26:18 +0200

    `reuse` gains the function `new_name()` (#11)
    Co-authored-by: Mauro Lepore <maurolepore@gmail.com>
    Co-authored-by: @lindadelacombaz

---
+++ R/new_name.R
@@ -0,0 +1,28 @@
+#' Rename an R object you're reusing
+#'
+#' @inheritParams reuse
+#' @param old,new String. The name of the old and new objects.
+#'
+#' @return The object stored as 'new'.
+#' @export
+#'
+#' @examples
+#' library(pins)
+#'
+#' board <- board_temp()
+#' x <- 1
+#'
+#' x %>% reuse("old", board)
+#' pin_exists("old", board = board)
+#'
+#' "old" %>% new_name("new", board)
+#' pin_exists("old", board = board)
+#' pin_exists("new", board = board)
+new_name <- function(old, new, board = board_reuse()) {
+  new_pin <- pins::pin_read(board, old)
+  new <- reuse(new_pin, new, board)
+
+  pins::pin_delete(board, old)
+
+  invisible(new)
+}
---
+++ tests/testthat/test-new_name.R
@@ -0,0 +1,27 @@
+test_that("takes a custom `board`", {
+  board <- pins::board_temp()
+  reuse(1, "old", board)
+  expect_no_error(new_name("old", "new", board = board))
+})
+
+test_that("uses a default board", {
+  default <- board_reuse()
+  withr::defer(pins::pin_delete(default, "new"))
+
+  reuse(1, "old")
+  new_name("old", "new")
+
+  expect_false(pins::pin_exists(default, "old"))
+  expect_true(pins::pin_exists(default, "new"))
+})
+
+test_that("replaces the old name with the new name", {
+  board <- pins::board_temp()
+  x <- 1
+  reuse(x, "old", board)
+
+  out <- new_name("old", "new", board)
+  expect_false(pins::pin_exists(board = board, "old"))
+  expect_true(pins::pin_exists(board = board, "new"))
+  expect_equal(out, x)
+})
